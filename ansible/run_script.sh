9cb03e32c3bb2a409126334e0842c774e8f81082
dew95531 <dew_95531@windowslive.com>
2018-05-06 22:28:17 +0700

    create ec2 instance , install nginx script done

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 access=$1
 secret=$2
@@ -6,7 +6,7 @@ secret=$2
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
-ansible-playbook ec2_webServer.yml
+ansible-playbook -vvv initial_ec2.yml --private-key=~/ec2-keys/webServer_rsa.pem
 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
