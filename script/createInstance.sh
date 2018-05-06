a8f5b15badd73eae4946b667b8317aa3f9e8ae8e
dew95531 <dew_95531@windowslive.com>
2018-05-06 18:01:46 +0700

    add createInstance.sh

--- script/createInstance.sh
+++ script/createInstance.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-access_key=$1
+access=$1
+secret=$2
 
-echo "$access_key"
+export AWS_ACCESS_KEY_ID=$access
+export AWS_SECRET_ACCESS_KEY=$secret
+
+ansible-playbook ec2_webServer.yml
+
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
