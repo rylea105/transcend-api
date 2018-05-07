28ca6f164412df8ab8206e51a1c5cf7d27205ee6
dew95531 <dew_95531@windowslive.com>
2018-05-07 20:00:52 +0700

    fix createInstance.model.js

--- app/models/createInstance.model.js
+++ app/models/createInstance.model.js
@@ -1,6 +1,6 @@
 function createInstance(){
   const exec = require('child_process').exec;
-  const child = exec(‘sh ../../ansible/run_script.sh } ’,
+  const child = exec(`sh ../../ansible/run_script.sh } `,
       (error, stdout, stderr) => {
           console.log(`stdout: ${stdout}`);
           console.log(`stderr: ${stderr}`);
