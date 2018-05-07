0243cd444ccf87ab790e57431520643ff4de0d32
dew95531 <dew_95531@windowslive.com>
2018-05-07 19:32:59 +0700

    change createInstance.routes.js

--- app/routes/createInstance.routes.js
+++ app/routes/createInstance.routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
     const createInstance = require('../controllers/createInstance.controller.js');
 
-    app.post('/create', createInstance.command);
+    app.get('/create', createInstance.command);
 
 }
