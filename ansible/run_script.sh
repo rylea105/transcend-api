0ef130f3a542687b21f51d3b4b031a44cc876b65
dew95531 <dew_95531@windowslive.com>
2018-05-07 21:15:39 +0700

    Update run_script.sh

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -6,6 +6,12 @@ secret=$2
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
+#sudo ssh-keygen -t rsa -f ~/ec2-keys/webServer_rsa -q -P "webServer"
+#sudo openssl rsa -in ~/ec2-keys/webServer_rsa -outform pem > ~/ec2-keys/webServer_rsa.pem
+#sudo chmod 600 ~/ec2-keys/webServer_rsa.pem
+
+ansible-playbook ec2_register_key.yml
+
 ansible-playbook initial_ec2.yml --private-key=~/ec2-keys/webServer_rsa.pem
 
 export AWS_ACCESS_KEY_ID=""
