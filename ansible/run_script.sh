6d03ffeedb486ad8d4ab3988058aba213e5f6eed
rylea105 <dew_95531@windowslive.com>
2018-08-30 21:49:47 +0700

    save

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -6,9 +6,9 @@ secret=$2
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
-#sudo ssh-keygen -t rsa -f ~/ec2-keys/webServer_rsa -q -P "webServer"
-#sudo openssl rsa -in ~/ec2-keys/webServer_rsa -outform pem > ~/ec2-keys/webServer_rsa.pem
-#sudo chmod 600 ~/ec2-keys/webServer_rsa.pem
+sudo ssh-keygen -t rsa -f ~/ec2-keys/webServer_rsa -q -P "webServer"
+sudo openssl rsa -in ~/ec2-keys/webServer_rsa -outform pem > ~/ec2-keys/webServer_rsa.pem
+sudo chmod 600 ~/ec2-keys/webServer_rsa.pem
 
 ansible-playbook /root/transcend-api/ansible/ec2_register_key.yml
 
