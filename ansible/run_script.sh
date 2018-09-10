b5908fa1821d8641250e115d21dc53eb773ce965
rylea105 <dew_95531@windowslive.com>
2018-09-10 20:13:46 +0700

    update script create keypair

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -6,13 +6,9 @@ secret=$2
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
-#sudo ssh-keygen -t rsa -f /root/ec2-keys/webServer_rsa -q -P "webServer"
-#sudo openssl rsa -in /root/ec2-keys/webServer_rsa -outform pem > /root/ec2-keys/webServer_rsa.pem
-#sudo chmod 755 /root/ec2-keys/webServer_rsa.pem
+ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
 
-#ansible-playbook /root/transcend-api/ansible/ec2_register_key.yml
-
-ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/etc/ssh/ssh_host_rsa_key
+ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem
 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
