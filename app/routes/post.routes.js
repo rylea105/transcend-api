a45536668230355a2765cace6a0feeda028eac7e
rylea105 <dew_95531@windowslive.com>
2018-09-20 23:48:10 +0700

    fix code

--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -2,6 +2,6 @@ module.exports = (app) => {
    const post = require('../controllers/post.controller.js');
 
    app.post('/post', post.add);
-   app.post('/post/:id', post.edit);
+   app.put('/post/:id', post.edit);
 }
 
