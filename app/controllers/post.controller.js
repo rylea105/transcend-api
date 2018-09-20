30e5af5b8bb659278c676207655c72a8dada638c
rylea105 <dew_95531@windowslive.com>
2018-09-20 23:37:38 +0700

    test

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -1,6 +1,9 @@
 const posts = require('../models/post.model.js');
 
 exports.add = (req, res) => {
-    posts.save(req.body);
-    res.send(req.body);
+    const addObj = new posts(req.body);
+    addObj.save(err =>{
+        if (err) return res.status(500).send(err);
+        return res.status(200).send(addObj);
+    })
 };
--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -2,6 +2,7 @@ module.exports = (app) => {
    const post = require('../controllers/post.controller.js');
 
    app.post('/post', post.add);
+   console.log("in routes")
 
 }
 
