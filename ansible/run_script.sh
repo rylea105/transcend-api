56cc0246375e06b124af501540407030cb79b323
rylea105 <dew_95531@windowslive.com>
2018-09-28 01:15:38 +0700

    change code

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -2,14 +2,14 @@
 
 access=$1
 secret=$2
-cicd=$3
+ci=$3
 
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
 ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
 
-ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem  --extra-vars cicd=$cicd
+ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem  -e cicd=$ci
 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
