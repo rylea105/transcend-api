4430e22108d1bfd0ffb44fff6a4eb2b4eac2f8ca
rylea105 <dew_95531@windowslive.com>
2018-09-27 22:19:53 +0700

    test variable in create instance script

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -2,13 +2,14 @@
 
 access=$1
 secret=$2
+cicd=$3
 
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
 ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
 
-ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem
+ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem --cicd=$cicd
 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
--- app/controllers/createInstance.controller.js
+++ app/controllers/createInstance.controller.js
@@ -1,10 +1,36 @@
 exports.command = (req, res) => {
-  const createInstance = require('../models/createInstance.model.js');
+    const region = "ap-southeast-1";
+    const keypair = req.body.keypair;
+    const instanceType = req.body.instanceType;
+    const image = req.boyy.image;
+    const group = req.body.group;
+    const subnetId = req.body.subnetId;
+    const cicd = req.body.cicd;
+    const code = req.body.code;
+    const monitor = req.body.monitor;
+    const access = req.body.access;
+    const secret = req.body.secret;
+
+    var spawn = require('child_process').spawn,
+    process    = spawn('sh',  ['/root/transcend-api/ansible/run_script.sh',]);
+
+    process.stdout.on('data', function (data) {
+      console.log('stdout: ' + data.toString());
+    });
+
+    process.stderr.on('data', function (data) {
+      console.log('stderr: ' + data.toString());
+    });
+
+    process.on('exit', function (code) {
+      console.log('child process exited with code ' + code.toString());
+    });
+
 };
 
 exports.test = (req,res) => {
   var name1 = req.body.name1;
-  
+
   var spawn = require('child_process').spawn,
   process    = spawn('sh',  ['/root/test.sh', name1, 'dodo']);
 
