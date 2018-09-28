fd16c119eb4026a45bba56c50622559ec9e464f7
rylea105 <dew_95531@windowslive.com>
2018-09-28 14:04:52 +0700

    fix variable

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -4,12 +4,12 @@ access=$1
 secret=$2
 ci=$3
 
-export AWS_ACCESS_KEY_ID="$access"
-export AWS_SECRET_ACCESS_KEY="$secret"
+#export AWS_ACCESS_KEY_ID="$access"
+#export AWS_SECRET_ACCESS_KEY="$secret"
 
 ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
 
-ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem  -e cicd="$ci"
+ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem  -e cicd=$ci
 
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
+#export AWS_ACCESS_KEY_ID=""
+#export AWS_SECRET_ACCESS_KEY=""
