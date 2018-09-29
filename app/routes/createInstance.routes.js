939f2673efe2bedcf500330aa4d570abe8a48370
rylea105 <dew_95531@windowslive.com>
2018-09-29 09:37:30 +0700

    remove /test

--- app/routes/createInstance.routes.js
+++ app/routes/createInstance.routes.js
@@ -2,5 +2,4 @@ module.exports = (app) => {
     const createInstance = require('../controllers/createInstance.controller.js');
 
     app.post('/create',createInstance.command);
-    app.post('/test',createInstance.test);
 }
