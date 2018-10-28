3da1982692a8c6d7539e714ecfc89c8f2f3cea1f
rylea105 <dew_95531@windowslive.com>
2018-10-28 21:47:37 +0700

    update var

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -16,21 +16,6 @@ echo $secret
 export AWS_ACCESS_KEY_ID="$access"
 export AWS_SECRET_ACCESS_KEY="$secret"
 
-#ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
-#ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "cicd=$cicd code=$code monitor=$monitor region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId"
-
-# if [ $software == "jenkins" ]
-# then
-# ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
-# ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "cicd=$cicd region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId"
-# fi
-
-# if [ $software == "gitlab" ]
-# then
-# ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
-# ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "code=$code region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId"
-# fi
-
 # if [ $software == "zabbix" ]
 # then
 # ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
@@ -40,6 +25,7 @@ export AWS_SECRET_ACCESS_KEY="$secret"
 echo $software
 
 ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
+
 ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "software=$software region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId"
 
 export AWS_ACCESS_KEY_ID=""
