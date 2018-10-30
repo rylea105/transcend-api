a0b3ce8fde8c755651548a9e00f600dd991d521b
rylea105 <dew_95531@windowslive.com>
2018-10-30 23:56:33 +0700

    edit log

--- app/controllers/log.controller.js
+++ app/controllers/log.controller.js
@@ -1,6 +1,6 @@
 const Log = require('../models/log.model.js');
 
-exports.findAll = function(res){
+exports.findAll = function(req, res){
     Log.find()
     .then(logs => {
         res.send(logs);
@@ -15,12 +15,4 @@ exports.post = function(req,res) {
         if (err) res.status(500).send(err);
     });
     return console.log(addObj);
-};
-
-exports.edit = function(req, res) {
-    Log.findOneAndUpdate( req.body.id, req.body, {new: true}, (err, editObj) => {
-            if (err) return res.status(500).send(err);
-            res.send(editObj);
-        }
-    )
 };
