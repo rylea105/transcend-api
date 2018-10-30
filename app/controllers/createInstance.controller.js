cf728d88ba90814c4401c3bb7d1793dbc1eb3fac
rylea105 <dew_95531@windowslive.com>
2018-10-30 22:22:17 +0700

    update createInstnace

--- app/controllers/createInstance.controller.js
+++ app/controllers/createInstance.controller.js
@@ -2,17 +2,7 @@ const fullstack = require("./fullstack.controller.js");
 const log = require("./log.controller.js");
 var shell = require('shelljs');
 
-exports.command = async (req, res) => {
-    var region = "ap-southeast-1";
-    var keypair = req.body.keypair;
-    var instanceType = req.body.instanceType;
-    var image = req.body.image;
-    var group = req.body.group;
-    var subnetId = req.body.subnetId;
-    var access = req.body.access;
-    var secret = req.body.secret;
-    var software = req.body.software;
-  
+exports.command = async (req, res) => {  
     req.body.ip = "-"
     req.body.status = "pending"
     
@@ -35,6 +25,16 @@ exports.preCreate = async (req,res) => {
 }
 
 exports.child_process = (req,res) => {
+  var region = "ap-southeast-1";
+  var keypair = req.body.keypair;
+  var instanceType = req.body.instanceType;
+  var image = req.body.image;
+  var group = req.body.group;
+  var subnetId = req.body.subnetId;
+  var access = req.body.access;
+  var secret = req.body.secret;
+  var software = req.body.software;
+
   var spawn = require('child_process').spawn,
   process = spawn('sh',  ['/root/transcend-api/ansible/run_script.sh',access,secret,region,keypair,instanceType,image,group,subnetId,software]);
 
