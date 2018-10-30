d30bc869ccd36db82faa1c36aaf0e946c3702c1d
rylea105 <dew_95531@windowslive.com>
2018-10-30 21:33:56 +0700

    change getoneprice: put  to post

--- app/routes/pricing.routes.js
+++ app/routes/pricing.routes.js
@@ -2,5 +2,5 @@ module.exports = (app) => {
     const pricing = require('../controllers/pricing.controller.js');
 
     app.get('/getallprice',pricing.findAll);
-    app.put('/getoneprice',pricing.findOneItem);
+    app.post('/getoneprice',pricing.findOneItem);
 }
