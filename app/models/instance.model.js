e81ab4d9954d0805b37afdd7108016da834a1116
rylea105 <dew_95531@windowslive.com>
2018-10-31 02:19:46 +0700

    add instance_id

--- app/models/instance.model.js
+++ app/models/instance.model.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const InstanceSchema = mongoose.Schema({
+    keypair: String,
     software: String,
     instanceType: String,
     region: String,
     image: String,
     group: String,
     ip: String,
+    instanceId: String,
     created: {type: Date, default: Date.now},
     status: String
 }, {
