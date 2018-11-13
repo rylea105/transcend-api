71b0a42074f655307784a26544cdd6f289fe2423
rylea105 <dew_95531@windowslive.com>
2018-11-14 01:12:37 +0700

    fix

--- app/controllers/instanceInfo.controller.js
+++ app/controllers/instanceInfo.controller.js
@@ -1,7 +1,4 @@
 const InstanceInfo = require('../models/instanceInfo.model.js');
-const app = express();
-var http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 exports.findAll = (req, res) => {
   InstanceInfo.find()
