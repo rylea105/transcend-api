0f03783f4d697efc7ee32e11146d83ce431a9d5a
rylea105 <dew_95531@windowslive.com>
2018-11-25 00:46:56 +0700

    fix srcipt

--- ansible/run_script.sh
+++ ansible/run_script.sh
@@ -11,7 +11,12 @@ subnetId=$8
 software=$9
 count=${10}
 
+export AWS_ACCESS_KEY_ID=$access
+export AWS_SECRET_ACCESS_KEY=$secret
 
 ansible-playbook /root/transcend-api/ansible/create_key-pair.yml
 
-ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "{access=$access secret=$secret software=$software region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId count=$count}"
+ansible-playbook /root/transcend-api/ansible/initial_fullstack.yml --private-key=/root/project.pem -e "{software=$software region=$region keypair=$keypair instance_type=$instanceType image=$image group=$group subnet_id=$subnetId count=$count}"
+
+export AWS_ACCESS_KEY_ID="access"
+export AWS_SECRET_ACCESS_KEY="secret"
