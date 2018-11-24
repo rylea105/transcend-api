ac0b916b0be68c0545d50203143d63190d12dbfb
rylea105 <dew_95531@windowslive.com>
2018-11-25 00:52:57 +0700

    fix software

--- app/controllers/createInstance.controller.js
+++ app/controllers/createInstance.controller.js
@@ -30,7 +30,7 @@ exports.child_process = async (req,res) => {
   var subnetId = req.body.subnetId;
   var access = req.body.access;
   var secret = req.body.secret;
-  var software = req.body.oneSoftware;
+  var software = req.body.Software;
   var count = req.body.count;
 
   console.log(software);
